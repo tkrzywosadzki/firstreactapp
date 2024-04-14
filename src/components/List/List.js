5c95e4a292ebef1d93e674b5a4ebf384c2eb1c7e
Tomasz Krzywosadzki <t.krzywosadzki@gmail.com>
2024-04-14 18:04:39 +0200

    add handleSubmit to List.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,8 +1,27 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
+import { useState, useEffect } from 'react';
+import shortid from 'shortid';
 
 
 const List = () => {
+
+    const [columns, setColumns] = useState([
+        { id: 1, title: 'Books', icon: 'book'},
+        { id: 2, title: 'Books', icon: 'film'},
+        { id: 3, title: 'Games', icon: 'gamepad'}
+    ]);
+
+    const [value, setValue] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setColumns([...columns, { id: shortid(), title: value}]);
+        setValue('');
+    };
+
+
+
     return (
         <div className="list">
             <header className={styles.header}>
@@ -10,10 +29,12 @@ const List = () => {
             </header>
             <p className={styles.description}>Interesting things I want to check out</p>
             <section className={styles.columns}>
-                <Column title="Books" icon="book" />
-                <Column title="Movies" icon="gamepad" />
-                <Column title="Games" icon="film" />
+                {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
             </section>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+                <button>Add column</button>
+            </form>
         </div>
     );
 };
