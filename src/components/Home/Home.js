dc31042e64c0e26b86cee3424c1e7387f0762d9f
Tomasz Krzywosadzki <t.krzywosadzki@gmail.com>
2024-05-07 21:16:51 +0200

    add 17.3.Etap 3

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,14 +2,14 @@ import styles from './Home.module.scss';
 import Hero from '../Hero/Hero';
 import SearchForm from '../SearchForm/SearchForm';
 import List from '../List/List';
-
+import Lists from '../Lists/Lists';
 
 const Home = () => {
     return (
         <>
         <Hero />
         <SearchForm />
-        <List />
+        <Lists />
         </>
     )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -10,6 +10,7 @@ export const getFilteredCards = ({ cards, searchString}, columnId) => cards
 export const getAllColumns = (state => state.columns);
 export const getListById = ({ lists }, listId) => lists.find(list => list.id === listId);
 export const getColumnsByList = ({columns}, listId) => columns.filter(column => column.listId === listId);
+export const getAllLists = (state => state.lists);
 
 // action creators
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
